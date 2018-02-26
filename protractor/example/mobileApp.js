9412d61e586c0328187145d03cc130d3dae0c1af
Niwes Tipboonchu <Niwesboy20@gmail.com>
2018-02-26 15:37:59 +0700

    Edit path enterprise
    Edit path enterprise

--- protractor/example/mobileApp.js
+++ protractor/example/mobileApp.js
@@ -6,17 +6,13 @@ exports.config = {
 	'deviceName' : 'vbox36p',
     'autoWebview': true,
 	'noReset': true,
-	'app' : 'C:\\Users\\lnw_a\\Desktop\\APK Android\\shop.apk'
-	// 'app' : 'C:\\Users\\lnw_a\\Desktop\\APK Android\\rmbk.apk'
+	'app' : 'path file apk'
 	
-	// 'app' : 'D:\\Tools Test\\Katalon\\programs\\apk\\shop.apk'
   },
   framework: 'jasmine',
-  // seleniumAddress: 'http://172.17.4.56:4723/wd/hub',
   seleniumAddress: 'http://localhost:4723/wd/hub',
   // specs: ['signIn.js','addItem.js','addReciept.js']
   specs: ['signIn.js']
-  // specs: ['loginRMBK.js']
   
   // 
 }
